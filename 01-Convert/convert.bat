b49bacc2d0769ee5ce7c92188ebdc88cfa32e949
Dale Stammen <dalestam@microsoft.com>
2017-05-22 18:25:45 -0700

    convert

---
+++ 01-Convert/convert.bat
@@ -0,0 +1,1 @@
+DesktopAppConverter.exe -Installer ".\01-Start\Binaries\Installer\FlightTracker.msi" -Destination ".\FlightTracker-AppX" -PackageName "FlightTracker" -Publisher "CN=AppConsult" -Version "1.0.0.0" -MakeAppx -Sign -Verbose
