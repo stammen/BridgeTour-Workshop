46e25c39d425f952b0d4acd8140f745e52a226e9
Dale Stammen <dalestam@microsoft.com>
2017-05-22 20:37:11 -0700

    fixed convert script

--- 01-Convert/convert.bat
+++ 01-Convert/convert.bat
@@ -1,1 +1,2 @@
-DesktopAppConverter.exe -Installer ".\01-Start\Binaries\Installer\FlightTracker.msi" -Destination ".\FlightTracker-AppX" -PackageName "FlightTracker" -Publisher "CN=AppConsult" -Version "1.0.0.0" -MakeAppx -Sign -Verbose
+DesktopAppConverter.exe -Installer "%~dp0\01-Start\Binaries\Installer\FlightTracker.msi" -Destination "%~dp0\FlightTracker-AppX" -PackageName "FlightTracker" -Publisher "CN=AppConsult" -Version "1.0.0.0" -MakeAppx -Sign -Verbose
+pause
